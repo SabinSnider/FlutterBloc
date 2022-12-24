c54e85f426941eb096010218bc4aa96b4d0bf250
SabinSnider <sabinvsic@gmail.com>
2022-12-24 17:22:20 +0545

    pizza_bloc added

--- lib/bloc/pizza_bloc.dart
+++ lib/bloc/pizza_bloc.dart
@@ -1,15 +1,38 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
-
 import '../models/pizza_model.dart';
-// import '../models/pizza_model.dart';
 part 'pizza_event.dart';
 part 'pizza_state.dart';
 
 class PizzaBloc extends Bloc<PizzaEvent, PizzaState> {
   PizzaBloc() : super(PizzaInitial()) {
-    on<PizzaEvent>((event, emit) {
-      // TODO: implement event handler
+    //when we start the application, this will be first event
+    on<LoadPizzaCounter>((event, emit) async {
+      await Future<void>.delayed(const Duration(seconds: 1));
+      emit(const PizzaLoaded(
+          pizzas: <Pizza>[])); //after waiting one sec, emit PizzaLoaded
+    });
+
+    on<AddPizza>((event, emit) {
+      if (state is PizzaLoaded) {
+        final state = this.state as PizzaLoaded;
+        emit(
+          PizzaLoaded(
+            pizzas: List.from(state.pizzas)..add(event.pizza),
+          ),
+        );
+      }
+    });
+
+    on<RemovePizza>((event, emit) {
+      if (state is PizzaLoaded) {
+        final state = this.state as PizzaLoaded;
+        emit(
+          PizzaLoaded(
+            pizzas: List.from(state.pizzas)..remove(event.pizza),
+          ),
+        );
+      }
     });
   }
 }
